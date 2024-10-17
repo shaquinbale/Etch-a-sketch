d53f8831212fe69a56cea8a9c53c0ae1b3d60582
shaquinbale <scopelenskingdra@gmail.com>
2024-10-17 13:22:14 -0400

    Add functionality to change pixel color to black

--- scripts.js
+++ scripts.js
@@ -1,19 +1,20 @@
 let canvasSize = 10;
-
-
+let canvas = document.querySelector("#canvas")
 
 function generateCanvas(canvasSize) {
-    let canvas = document.querySelector("#canvas")
-
-
-
     for (let boxes = 0; boxes < canvasSize ** 2; boxes++) {
         let pixel = document.createElement("div");
         pixel.style.width = `${500 / canvasSize}px`;
         pixel.style.height = `${500 / canvasSize}px`;
         pixel.style.backgroundColor = 'White';
+        pixel.setAttribute('id', `${boxes}`)
         canvas.appendChild(pixel);
     }
 }
+
+canvas.addEventListener('click', (event) => {
+    const pixel = document.getElementById(event.target.id);
+    pixel.style.backgroundColor = 'black';
+})
 
 generateCanvas(canvasSize);
