0c2187e22a7e3d3d3fa5b4da4ec08094b9a3c5c1
shaquinbale <scopelenskingdra@gmail.com>
2024-10-17 12:57:24 -0400

    Add function to generate canvas pixels

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
-let canvas = document.querySelector("#canvas")
+let canvasSize = 10;
+
+
+
+function generateCanvas(canvasSize) {
+    let canvas = document.querySelector("#canvas")
+
+
+
+    for (let boxes = 0; boxes < canvasSize ** 2; boxes++) {
+        let pixel = document.createElement("div");
+        pixel.style.width = `${500 / canvasSize}px`;
+        pixel.style.height = `${500 / canvasSize}px`;
+        pixel.style.backgroundColor = 'White';
+        canvas.appendChild(pixel);
+    }
+}
+
+generateCanvas(canvasSize);
